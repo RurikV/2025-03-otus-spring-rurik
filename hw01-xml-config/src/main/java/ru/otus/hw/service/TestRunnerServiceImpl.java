439c5689771ae064abae7130b9e2385d07f0ee45
Vladimir Rurik <vladimir.rurik@gmail.com>
2025-05-05 22:05:18 +0300

    Handle exceptions in TestRunnerServiceImpl during test execution

--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @RequiredArgsConstructor
 public class TestRunnerServiceImpl implements TestRunnerService {
@@ -10,9 +11,19 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     @Override
     public void run() {
-        String firstName = ioService.readStringWithPrompt("Please enter your first name:");
-        String lastName = ioService.readStringWithPrompt("Please enter your last name:");
+        try {
+            String firstName = ioService.readStringWithPrompt("Please enter your first name:");
+            String lastName = ioService.readStringWithPrompt("Please enter your last name:");
 
-        testService.executeTestForStudent(firstName, lastName);
+            testService.executeTestForStudent(firstName, lastName);
+        } catch (QuestionReadException e) {
+            ioService.printLine("Error: Unable to read test questions.");
+            ioService.printLine("Please check that the questions file exists and is properly formatted.");
+            ioService.printFormattedLine("Technical details: %s", e.getMessage());
+        } catch (Exception e) {
+            ioService.printLine("Error: An unexpected error occurred while running the test.");
+            ioService.printLine("Please contact the administrator for assistance.");
+            ioService.printFormattedLine("Technical details: %s", e.getMessage());
+        }
     }
 }
